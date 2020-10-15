98512bdcfd1ddf446e7c9953791310aa72540b2a
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2020-10-16 01:45:15 +0300

    user photo has been changed.

--- resources/views/layouts/admin_panel.blade.php
+++ resources/views/layouts/admin_panel.blade.php
@@ -61,7 +61,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-4 d-flex">
                 <div class="image">
-                    <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('images/userprofileimg.png')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="dropdown mt-3 pb-3 mb-3 d-flex ">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
